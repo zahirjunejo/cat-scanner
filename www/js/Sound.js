83c4776a31140fa84e06c5b282f72c83e3557239
Zahir Junejo <zjunejo1993@gmail.com>
2017-01-05 05:09:58 +0500

    Some changes to config and created Sound class, es6 is great

---
+++ www/js/Sound.js
@@ -0,0 +1,25 @@
+class Sound
+{
+    
+	constructor(urlOfSound){
+		this._url = urlOfSound;
+		// Create an audio element
+		// Set its src as the urlOfSound
+		// Adds the element to the body
+	}
+
+	play(){
+       
+	}
+
+	pause(){
+
+	}
+
+	get soundName(){
+
+	} 
+
+
+
+}
